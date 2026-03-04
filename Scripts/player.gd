ddb86571057ee75402604b3f31d90d10ca32773e
Limidan <limidan7@gmail.com>
2026-03-04 21:18:18 +0000

    pluh

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,5 @@
 class_name Player extends CharacterBody2D
-
+var move_changes = 0
 var current_lane = 0
 var lane_position = [60, 180,300]
 var bullet_scene = preload("res://Scenes/bullet.tscn")
@@ -29,6 +29,8 @@ func spawn_bullets():
 		var bullet_instance = bullet_scene.instantiate()
 		print("hello")
 		add_child(bullet_instance)
-		bullet_instance.global_position.x = global_position.x
-	
-	
+		if move_changes == 0:
+			bullet_instance.global_position.x = global_position.x
+			move_changes += 1
+		else:
+			print("pluh")
