7581c9b0367a2d3de1c0a3c2a9a7608ff695432d
Limidan <limidan7@gmail.com>
2026-03-04 21:15:54 +0000

    added bullets
    yk

--- Scripts/missile.gd
+++ Scripts/missile.gd
@@ -22,3 +22,4 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		queue_free()
+		
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 var current_lane = 0
 var lane_position = [60, 180,300]
+var bullet_scene = preload("res://Scenes/bullet.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position.x 
@@ -15,8 +16,19 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Left_Move") and current_lane >0:
 		current_lane -= 1
 	
-	
+	shoot()
 	position.x = lane_position[current_lane]
 	
+	
+func shoot():
+	if Input.is_action_just_pressed("Shoot"):
+		spawn_bullets()
+	
+	
+func spawn_bullets():
+		var bullet_instance = bullet_scene.instantiate()
+		print("hello")
+		add_child(bullet_instance)
+		bullet_instance.global_position.x = global_position.x
 	
 	
